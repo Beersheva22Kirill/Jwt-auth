f851d3d52f043596fdbbf28ee13dc92e7b8c07c8
kirill2884 <kirill2884@gmail.com>
2023-08-16 09:04:17 +0300

    Add log info

--- src/main/java/telran/spring/security/SecurityConfiguration.java
+++ src/main/java/telran/spring/security/SecurityConfiguration.java
@@ -1,20 +1,20 @@
 package telran.spring.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import telran.spring.security.jwt.JwtFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class SecurityConfiguration {
 
 	final JwtFilter jwtFilter;
@@ -26,7 +26,9 @@ public class SecurityConfiguration {
 		http.cors(custom -> custom.disable()).csrf(custom -> custom.disable())
 				.authorizeHttpRequests(custom -> custom.requestMatchers("/login").permitAll());
 		authorizationConfiguration.configure(http);
+		log.info("Filter with JWToken - configured and loaded");
 		return http.httpBasic(Customizer.withDefaults())
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
+		
 	}
 }
